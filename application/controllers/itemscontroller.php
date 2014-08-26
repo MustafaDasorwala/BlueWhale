4e271f30c521695a9ce77de7402f2a09e075f6cc
MustafaDasorwala <mustafa.dasorwala@yahoo.com>
2014-08-26 19:56:53 -0400

    mysqli used instead of mysql

--- application/controllers/itemscontroller.php
+++ application/controllers/itemscontroller.php
@@ -2,28 +2,27 @@
 
 class ItemsController extends Controller {
 
-	function view($id = null,$name = null) {
-	
-		$this->set('title',$name.' - My Todo List App');
-		$this->set('todo',$this->Item->select($id));
+    function view($id = null, $name = null) {
 
-	}
-	
-	function viewall() {
+        $this->set('title', $name . ' - My Todo List App');
+        $this->set('todo', $this->Item->select($id));
+    }
 
-		$this->set('title','All Items - My Todo List App');
-		$this->set('todo',$this->Item->selectAll());
-	}
-	
-	function add() {
-		$todo = $_POST['todo'];
-		$this->set('title','Success - My Todo List App');
-		$this->set('todo',$this->Item->query('insert into items (item_name) values (\''.mysql_real_escape_string($todo).'\')'));	
-	}
-	
-	function delete($id = null) {
-		$this->set('title','Success - My Todo List App');
-		$this->set('todo',$this->Item->query('delete from items where id = \''.mysql_real_escape_string($id).'\''));	
-	}
+    function viewall() {
+
+        $this->set('title', 'All Items - My Todo List App');
+        $this->set('todo', $this->Item->selectAll());
+    }
+
+    function add() {
+        $todo = $_POST['todo'];
+        $this->set('title', 'Success - My Todo List App');
+        $this->set('todo', $this->Item->query('insert into items (item_name) values (\'' . mysql_real_escape_string($todo) . '\')'));
+    }
+
+    function delete($id = null) {
+        $this->set('title', 'Success - My Todo List App');
+        $this->set('todo', $this->Item->query('delete from items where id = \'' . mysql_real_escape_string($id) . '\''));
+    }
 
 }
--- library/sqlquery.class.php
+++ library/sqlquery.class.php
@@ -1,97 +1,89 @@
 <?php
 
 class SQLQuery {
+
     protected $_dbHandle;
     protected $_result;
 
-    /** Connects to database **/
-
+    /** Connects to database * */
     function connect($address, $account, $pwd, $name) {
-        $this->_dbHandle = @mysql_connect($address, $account, $pwd);
+        $this->_dbHandle = mysqli_connect($address, $account, $pwd, $name);
         if ($this->_dbHandle != 0) {
-            if (mysql_select_db($name, $this->_dbHandle)) {
+            if (mysqli_select_db($this->_dbHandle, $name)) {
                 return 1;
-            }
-            else {
+            } else {
                 return 0;
             }
-        }
-        else {
+        } else {
             return 0;
         }
     }
 
-    /** Disconnects from database **/
-
+    /** Disconnects from database * */
     function disconnect() {
-        if (@mysql_close($this->_dbHandle) != 0) {
+        if (mysqli_close($this->_dbHandle) != 0) {
             return 1;
-        }  else {
+        } else {
             return 0;
         }
     }
-    
+
     function selectAll() {
-    	$query = 'select * from `'.$this->_table.'`';
-    	return $this->query($query);
+        $query = 'select * from `' . $this->_table . '`';
+        return $this->query($query);
     }
-    
+
     function select($id) {
-    	$query = 'select * from `'.$this->_table.'` where `id` = \''.mysql_real_escape_string($id).'\'';
-    	return $this->query($query, 1);    
+        $query = 'select * from `' . $this->_table . '` where `id` = \'' . mysqli_real_escape_string($id) . '\'';
+        return $this->query($query, 1);
     }
 
-	
-    /** Custom SQL Query **/
+    /** Custom SQL Query * */
+    function query($query, $singleResult = 0) {
 
-	function query($query, $singleResult = 0) {
+        $this->_result = mysqli_query($query, $this->_dbHandle);
 
-		$this->_result = mysql_query($query, $this->_dbHandle);
+        if (preg_match("/select/i", $query)) {
+            $result = array();
+            $table = array();
+            $field = array();
+            $tempResults = array();
+            $numOfFields = mysqli_num_fields($this->_result);
+            for ($i = 0; $i < $numOfFields; ++$i) {
+                array_push($table, mysqli_field_table($this->_result, $i));
+                array_push($field, mysqli_field_name($this->_result, $i));
+            }
 
-		if (preg_match("/select/i",$query)) {
-		$result = array();
-		$table = array();
-		$field = array();
-		$tempResults = array();
-		$numOfFields = mysql_num_fields($this->_result);
-		for ($i = 0; $i < $numOfFields; ++$i) {
-		    array_push($table,mysql_field_table($this->_result, $i));
-		    array_push($field,mysql_field_name($this->_result, $i));
-		}
 
-		
-			while ($row = mysql_fetch_row($this->_result)) {
-				for ($i = 0;$i < $numOfFields; ++$i) {
-					$table[$i] = trim(ucfirst($table[$i]),"s");
-					$tempResults[$table[$i]][$field[$i]] = $row[$i];
-				}
-				if ($singleResult == 1) {
-		 			mysql_free_result($this->_result);
-					return $tempResults;
-				}
-				array_push($result,$tempResults);
-			}
-			mysql_free_result($this->_result);
-			return($result);
-		}
-		
+            while ($row = mysqli_fetch_row($this->_result)) {
+                for ($i = 0; $i < $numOfFields; ++$i) {
+                    $table[$i] = trim(ucfirst($table[$i]), "s");
+                    $tempResults[$table[$i]][$field[$i]] = $row[$i];
+                }
+                if ($singleResult == 1) {
+                    mysqli_free_result($this->_result);
+                    return $tempResults;
+                }
+                array_push($result, $tempResults);
+            }
+            mysqli_free_result($this->_result);
+            return($result);
+        }
+    }
 
-	}
-
-    /** Get number of rows **/
+    /** Get number of rows * */
     function getNumRows() {
-        return mysql_num_rows($this->_result);
+        return mysqli_num_rows($this->_result);
     }
 
-    /** Free resources allocated by a query **/
-
+    /** Free resources allocated by a query * */
     function freeResult() {
-        mysql_free_result($this->_result);
+        mysqli_free_result($this->_result);
     }
 
-    /** Get error string **/
-
+    /** Get error string * */
     function getError() {
-        return mysql_error($this->_dbHandle);
+        return mysqli_error($this->_dbHandle);
     }
+
 }
